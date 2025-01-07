bae93b0aa7a71b8c2cd4fd059ec6a206275c2b7e
Alexiia99 <alexiahj@hotmail.es>
2025-01-07 20:34:37 +0100

    Error 500  al crear el get del postman

--- public/index.php
+++ public/index.php
@@ -13,5 +13,10 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Cargar rutas personalizadas
+require __DIR__.'/../routes/api.php'; // Añade esta línea para cargar las rutas API
+
+// Manejar la solicitud
+$app->handleRequest(Request::capture());
